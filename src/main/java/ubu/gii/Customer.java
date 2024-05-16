5b52274d5c3144d3a27c976b2c8bae886b279a13
Marcos <muf1002@alu.ubu.es>
2024-05-16 12:46:45 +0200

    #2 Clean comments and remove not used var totalAmount

--- src/main/java/ubu/gii/Customer.java
+++ src/main/java/ubu/gii/Customer.java
@@ -31,21 +31,16 @@ public class Customer {
 	};
 
 	public String statement() {
-		double totalAmount = 0;
 		int frequentRenterPoints = 0;
 		Iterator<Rental> rentals = _rentals.iterator();
 		String result = "Rental Record for " + getName() + "\n";
 		while (rentals.hasNext()) {
 			Rental each = rentals.next();
-
-			
-			// add frequent renter points
 			frequentRenterPoints = each.getFrequentRenterPoints();
-			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t"
 					+ String.valueOf(each.getCharge()) + "\n";
 		}
-		// add footer lines
+		
 		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
 		result += "You earned " + String.valueOf(frequentRenterPoints)
 				+ " frequent renter points";
