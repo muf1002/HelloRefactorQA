fa75938290ca9ed06a7a55dbe5b6fa1301c7ac59
dmp1002 <dmp1002@alu.ubu.es>
2024-05-16 13:25:27 +0200

    #3 Move Rental.getCharge Rental to Movie

--- src/main/java/ubu/gii/Rental.java
+++ src/main/java/ubu/gii/Rental.java
@@ -27,24 +27,8 @@ public class Rental {
 		return _movie;
 	}
 
-	double getCharge() {
-		double result=0;
-		switch (getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if (getDaysRented() > 2)
-				result += (getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			result += getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			result += 1.5;
-			if (getDaysRented() > 3)
-				result += (getDaysRented() - 3) * 1.5;
-			break;
-		}
-		return result;
+	public double getCharge() {
+		return _movie.getCharge(this.getDaysRented());
 	}
 
 	int getFrequentRenterPoint() {
